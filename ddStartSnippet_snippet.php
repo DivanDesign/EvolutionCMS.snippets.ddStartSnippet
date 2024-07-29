af41ec17bfb63feaa0548746de79539f6f9062b0
Ilyas Ronef <ilyas@DivanDesign.ru>
2024-07-29 14:49:55 +0400

    * Minor changes (code style).

--- ddStartSnippet_snippet.php
+++ ddStartSnippet_snippet.php
@@ -12,8 +12,8 @@
 
 //Include (MODX)EvolutionCMS.libraries.ddTools
 require_once(
-	$modx->getConfig('base_path') .
-	'assets/libs/ddTools/modx.ddtools.class.php'
+	$modx->getConfig('base_path')
+	. 'assets/libs/ddTools/modx.ddtools.class.php'
 );
 
 return \DDTools\Snippet::runSnippet([
